b3763acdcabbe461478b3af88d3a5d299e528ab4
cck <scirelli+cck@gmail.com>
2022-08-08 19:07:23 +0000

    Successful switch read

--- src/main.py
+++ src/main.py
@@ -1,16 +1,16 @@
 #!/usr/bin/env python3
-
-from time import sleep
-
-from Adafruit_BBIO.GPIO import GPIO
+from Adafruit_BBIO import GPIO
 
 GPIO.setup("P8_12", GPIO.IN)
 
 
 def main() -> None:
+    old_pin_state = 0
     while True:
-        print(GPIO.input("P8_12"))
-        sleep(0.1)
+        new_pin_state = GPIO.input("P8_12")
+        if new_pin_state != old_pin_state:
+            print("button pressed")
+            old_pin_state = new_pin_state
 
 
 if __name__ == "__main__":
