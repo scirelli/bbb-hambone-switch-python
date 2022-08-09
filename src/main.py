110c42788ca232d5f382bac108f22700fad69909
scirelli <scirelli+git@gmail.com>
2022-08-09 12:34:57 -0400

    Adding code for all switches, test.

--- src/main.py
+++ src/main.py
@@ -1,15 +1,31 @@
 #!/usr/bin/env python3
 from Adafruit_BBIO import GPIO  # pylint: disable=no-name-in-module
 
-GPIO.setup("P8_12", GPIO.IN, pull_up_down=GPIO.PUD_UP)
+FRONT_LIMIT_SWITCH_PIN = "P8_12"
+REAR_LIMIT_SWITCH_PIN = "P8_10"
+DOOR_SWITCH_PIN = "P8_8"
+
+GPIO.setup("P8_12", GPIO.IN, pull_up_down=GPIO.PUD_UP)  # Front limit switch
+GPIO.setup("P8_10", GPIO.IN, pull_up_down=GPIO.PUD_UP)  # Rear limit switch
+GPIO.setup("P8_8", GPIO.IN, pull_up_down=GPIO.PUD_UP)  # Door switch
 
 
 def main() -> None:
     while True:
         if GPIO.input("P8_12"):
-            print("button released")
+            print("Front limit switch released")
         else:
-            print("button pressed")
+            print("Front limit switch pressed")
+
+        if GPIO.input("P8_10"):
+            print("Rear limit switch released")
+        else:
+            print("Rear limit switch pressed")
+
+        if GPIO.input("P8_8"):
+            print("Door switch released")
+        else:
+            print("Door switch pressed")
 
 
 if __name__ == "__main__":
