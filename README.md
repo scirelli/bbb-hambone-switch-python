a1292684ab57f9ea5766aa433c47332a543ef206
scirelli <scirelli+git@gmail.com>
2022-08-09 12:25:25 -0400

    Enabled the pins pull up resistor. Change output

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # BBB Hamebone Reading a switch
 https://learn.adafruit.com/setting-up-io-python-library-on-beaglebone-black
+
+## Setup
+Adafruit_BBIO is not installable on anything but an ARM system. To allow mypy and linting to work the `.venv/lib/python3.10/site-packages` Adafruit_BBIO packages have been tar'd and checked into this project.
+For development not on the BBB you can comment out the Adafruit_BBBIO package in the Pipfile, do an normal install `make install-dev && make install-precommit` then extract the tar into the site-packages folder.
--- src/main.py
+++ src/main.py
@@ -1,17 +1,20 @@
 #!/usr/bin/env python3
-from Adafruit_BBIO import GPIO
+from Adafruit_BBIO import GPIO  # pylint: disable=no-name-in-module
 
-GPIO.setup("P8_12", GPIO.IN)
+GPIO.setup("P8_12", GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 
 def main() -> None:
-    old_pin_state = 0
     while True:
-        new_pin_state = GPIO.input("P8_12")
-        if new_pin_state != old_pin_state:
+        if GPIO.input("P8_12"):
+            print("button released")
+        else:
             print("button pressed")
-            old_pin_state = new_pin_state
 
 
 if __name__ == "__main__":
-    main()
+    try:
+        main()
+    except BaseException:
+        GPIO.cleanup()
+        raise
