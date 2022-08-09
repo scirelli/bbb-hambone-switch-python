514b9093e4833033494febf78de7a9672d634bf5
scirelli <scirelli+git@gmail.com>
2022-08-09 13:30:34 -0400

    Adding some motor driving code

--- src/main.py
+++ src/main.py
@@ -4,14 +4,16 @@ from Adafruit_BBIO import GPIO  # pylint: disable=no-name-in-module
 FRONT_LIMIT_SWITCH_PIN = "P8_12"
 REAR_LIMIT_SWITCH_PIN = "P8_10"
 DOOR_SWITCH_PIN = "P8_8"
+MOTOR_IN1_PIN = "P8_7"  # Drive backward (toward BBB)
+MOTOR_IN2_PIN = "P8_9"  # Drive forward (away from BBB)
 
-GPIO.setup(
-    FRONT_LIMIT_SWITCH_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP
-)  # Front limit switch
-GPIO.setup(
-    REAR_LIMIT_SWITCH_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP
-)  # Rear limit switch
-GPIO.setup(DOOR_SWITCH_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)  # Door switch
+GPIO.setup(FRONT_LIMIT_SWITCH_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(REAR_LIMIT_SWITCH_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(DOOR_SWITCH_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(DOOR_SWITCH_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+GPIO.setup(MOTOR_IN1_PIN, GPIO.OUT, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(MOTOR_IN2_PIN, GPIO.OUT, pull_up_down=GPIO.PUD_DOWN)
 
 
 def main() -> None:
@@ -20,16 +22,28 @@ def main() -> None:
             print("Front limit switch released")
         else:
             print("Front limit switch pressed")
+            driveBackward()
 
         if GPIO.input(REAR_LIMIT_SWITCH_PIN):
             print("Rear limit switch released")
         else:
             print("Rear limit switch pressed")
+            driveForward()
 
         if GPIO.input(DOOR_SWITCH_PIN):
             print("Door switch released")
         else:
             print("Door switch pressed")
+
+
+def driveForward() -> None:
+    GPIO.output(MOTOR_IN1_PIN, GPIO.LOW)
+    GPIO.output(MOTOR_IN2_PIN, GPIO.HIGH)
+
+
+def driveBackward() -> None:
+    GPIO.output(MOTOR_IN2_PIN, GPIO.LOW)
+    GPIO.output(MOTOR_IN1_PIN, GPIO.HIGH)
 
 
 if __name__ == "__main__":
